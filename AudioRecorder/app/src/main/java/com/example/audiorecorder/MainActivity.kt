dd7d242f24326244f70f5dd85a64861ab0d7998d
avaizqaiser2002-del <avaizqaiser2002@gmail.com>
2026-04-23 08:52:04 +0500

    Update: 2026-04-23 08:52

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -204,8 +204,6 @@ class MainActivity : AppCompatActivity() {
     private fun hasPermissions(): Boolean {
         val required = buildList {
             add(Manifest.permission.RECORD_AUDIO)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-                add(Manifest.permission.POST_NOTIFICATIONS)
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
                 add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
